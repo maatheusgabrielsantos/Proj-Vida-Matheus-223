15355cb606e4e21bf609016ba9e3bc81088536da
maatheusgabrielsantos <128188384+maatheusgabrielsantos@users.noreply.github.com>
2024-04-11 17:29:18 -0300

    11/04

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
         for (let j = 0; j < botoes.length; j++) {
@@ -12,4 +11,20 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+
+
+
+
+
+
+
 }
